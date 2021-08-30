c60585444e5b9b74f4b2e259fd0d2b72b5e8391a
henk jurriens <henk.jurriens@gmail.com>
2021-08-30 10:41:39 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,9 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+
+
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { App } from "@capacitor/app";
+import { Clipboard } from "@capacitor/clipboard";
+import React from "react";
+
+const change = async () => {
+  App.addListener("appStateChange", async ({ isActive }) => {
+    console.log("App state changed. Is active?", isActive);
+    const { value } = await Clipboard.read();
+    alert(value);
+  });
+};
 
 const Home: React.FC = () => {
+  React.useEffect(() => {
+    change();
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Share Demo</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Share Demo</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
